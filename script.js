33b2d9b9d3a6ed901248968bf32e4af16b6f335c
amalendu02 <amalendumaiti14@gmail.com>
2024-07-28 22:03:50 +0530

    curser,thumb-done

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const lowercaseCheck = document.querySelector("#lowercase");
 const numbersCheck = document.querySelector("#numbers");
 const symbolsCheck = document.querySelector("#symbols");
 const indicator = document.querySelector("[data-indicator]");
-const generateBtn = document.querySelector(".generateButton");
+const generateBtn = document.querySelector(".generateBtn");
 const allCheckBox = document.querySelectorAll("input[type = checkbox]");
 const symbols = `~!@#$%^&*(){}_+-=|][,<.>/?;:'"`;
 
@@ -17,22 +17,30 @@ let passwordLength = 10;
 let checkCount = 0;
 handleSlider();
 
+// set strength color to grey
+setIndecator("#ccc")
+
 
 // Set Password Length
 function handleSlider() {
     inputSlider.value = passwordLength;
     lengthDisplay.innerText = passwordLength;
+
+    const min = inputSlider.min;
+    const max = inputSlider.max;
+    inputSlider.style.backgroundSize = ((passwordLength - min)*100/(max - min) +"% 100%") 
 }
 
 function setIndecator(color) {
     indicator.style.backgroundColor = color;
-
     // shado
+    indicator.style.boxShadow = `0px 0px 12px 1px ${color}`;
 }
 
 function getRndInteger(min,max) {
     return Math.floor(Math.random() * (max - min)) +min;
 }
+
 
 function geneareLowerCase() {
     return String.fromCharCode(getRndInteger(97,123));
@@ -79,12 +87,13 @@ function calcStrength() {
 
 async function copyContent() {
     try {
-        await Navigator.clipboard.writeText(passwordDisplay.value);
+        await navigator.clipboard.writeText(passwordDisplay.value);
         copyMsg.innerText = "copied";
     }
     catch(e) {
         copyMsg.innerText = "Faield";
     }
+    // to make copy span visible
     copyMsg.classList.add("active");
 
     setTimeout( () => {
@@ -95,8 +104,11 @@ async function copyContent() {
 
 
 function shufflePassword(array) {
-    for(let i = array.length - 1; i>0; i--) {
+    // Fisher Yates Method  (we use this method to suflling)
+    for(let i = array.length - 1; i > 0; i--) {
+        // random j, find out using random function
         const j = Math.floor(Math.random() * (i+1));
+        //swap number at i index and j index
         const temp = array[i];
         array[i] = array[j];
         array[j] = temp;
@@ -135,7 +147,8 @@ copyBtn.addEventListener('click', () => {
 })
 
 generateBtn.addEventListener('click', () => {
-    if(checkCount ==0)
+
+    if(checkCount == 0)
     return;
     
     if(passwordLength < checkCount) {
@@ -163,7 +176,7 @@ generateBtn.addEventListener('click', () => {
     //     password += generateRandomNumber();
     // }
 
-     let funArr = [];
+     let funArr = [];   // in this function we going to store uppercase, lowercase, randomnumber 
 
      if(uppercaseCheck.checked)
         funArr.push(generateUpperCase);
